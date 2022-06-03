a75764e729ff8ee689871a0f51485a11123761c1
jbellon <josh@joshbellon.com>
2022-06-02 22:09:44 -0700

    homework part 3 is completed and part 4 is started up til step number 3

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,17 +1,23 @@
 import './NewSkillForm.css';
+import { useState } from "react";
 
 function NewSkillForm() {
+    const [formSkills, setFormSkills] = useState({
+        name: "",
+        level: 3
+    })
+
     return (
         <form className="NewSkillForm">
             <label>Skill</label>
-            <input type="textfield"></input>
+            <input value={formSkills.name} onChange={(evt) => setFormSkills(evt.target.value)} type="textfield"></input>
             <label>Level</label>
-            <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+            <select value={formSkills.level} onChange={(evt) => setFormSkills(evt.target.value)}>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
             </select>
             <button type="submit">ADD SKILL</button>
         </form>
